c44d9d9b6d09e670974c32931f0bb21a4a32c0ec
ayushvasu <ayushvasu98@gmail.com>
2018-06-28 17:35:18 +0530

    Update trait_mixins_monads-examples.scala

--- trait_mixins_monads-examples.scala
+++ trait_mixins_monads-examples.scala
@@ -1,3 +1,16 @@
+class Util {
+  val nanoSecsInMilliSec = 1000000L
+  def profile[R](codeBlock: => R): (R, Long) = {
+  		println("startstarte")
+      val t0 = System.nanoTime()
+      val result = codeBlock
+      (result, (System.nanoTime() - t0)/nanoSecsInMilliSec)
+  }
+  
+}
+
+
+
 //Trait and Mixins
 trait Friend {
 	val name:String
