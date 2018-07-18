5b17bca173f42faa5d0586d2346251b7fbf38166
ayushvasu <ayush.vasu@impetus.co.in>
2018-07-18 15:50:53 +0530

    Local maven repo

--- SBTExample/project/Resolvers.scala
+++ SBTExample/project/Resolvers.scala
@@ -4,5 +4,9 @@ import Keys._
 object Resolvers {
 
   val tconfrepo = "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
+  
+  //for local maven repo m2
+  val localM2 = "Local Maven Repository" at "file://"+Path.userHome.absolutePath+"/.m2/repository"
+
 
 }
