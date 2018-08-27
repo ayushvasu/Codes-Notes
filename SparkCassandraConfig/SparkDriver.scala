2d4417a74bc0f8451dfbd41ada2007d553f770f9
Ayush Vasu <ayush.vasu@impetus.co.in>
2018-08-27 15:31:08 +0530

    Create SparkDriver.scala

---
+++ SparkCassandraConfig/SparkDriver.scala
@@ -0,0 +1,22 @@
+package com.glassbeam.s_c2r
+
+import org.slf4j.LoggerFactory
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SparkSession
+
+
+class SparkDriver(conf: Config) {
+  val log = LoggerFactory.getLogger(classOf[SparkDriver])
+
+  lazy val sparkConf = new SparkConf().setAll(conf.spark)
+  lazy val sparkSession = SparkSession.builder().config(sparkConf).getOrCreate()
+                  
+  def init(): SparkSession = sparkSession
+  
+  def shutdown(): Unit = {
+    sparkSession.stop()
+  }
+
+}
+
