21529e1a845f17cbeabd95bfe34ca083ffddfe74
Ayush Vasu <ayush.vasu@impetus.co.in>
2018-09-15 11:48:53 +0530

    Update FututreExample1.scala

--- FututreExample1.scala
+++ FututreExample1.scala
@@ -103,3 +103,19 @@ import java.util.concurrent.Executors
 import concurrent.ExecutionContext
 val executorService = Executors.newFixedThreadPool(4)
 val executionContext = ExecutionContext.fromExecutorService(executorService)
+
+//Wait for all future to complete before stop vm
+import scala.concurrent._
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
+
+val f1 = Future(Thread.sleep(1000))
+val f2 = Future(Thread.sleep(2000))
+val all = Future.sequence(Seq(f1, f2))
+all.foreach(_ => println("All done"))
+
+f1.foreach(_ => println("Future 1 done"))
+f2.foreach(_ => println("Future 2 done"))
+
+// Wait for the futures to complete before shutting down
+Await.result(all, 5.seconds)
