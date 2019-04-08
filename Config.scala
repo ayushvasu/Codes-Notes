8e1b024c8747a7bc6381ecaf06aa24c3cfca49db
Ayush Vasu <ayush.vasu@impetus.co.in>
2019-04-08 14:20:18 +0530

    config read

---
+++ Config.scala
@@ -0,0 +1,44 @@
+  /*
+	#application.conf
+  
+  metadata {
+    driver="org.postgresql.Driver"
+    url="jdbc:postgresql://127.0.0.1:5432/ayushTest"
+    user="postgres"
+    password="root"
+    schema="dwmetadata"
+  }
+
+  cache {
+    enable = false
+    retention-Period = 24 hours
+    //Sources to cached - LOGICAL, S3, HDFS, JDBC
+    sources = ["LOGICAL","S3"]
+  }
+  
+  */
+
+
+  
+  import com.typesafe.config.ConfigFactory
+
+  import java.util.concurrent.TimeUnit._
+
+  import scala.collection.JavaConverters._
+
+  val config = ConfigFactory.load()
+
+  val app = config.getConfig("application")
+
+  //list = ["a1","a2"]
+  val list = app.getStringList("list").asScala.toList.map(_.toUpperCase())
+
+  val metadata = app.getObject("metadata").unwrapped().asScala.toList.map {case(k,v) => ("metadata."+k, v.asInstanceOf[String])}.toMap
+
+  val cacheConfig = app.getConfig("cache")
+
+  lazy val retaintionPeriod = cacheConfig.getDuration("retention-Period", SECONDS)
+
+  lazy val cacheEnable = cacheConfig.getBoolean("enable")
+
+  lazy val cacheSources = cacheConfig.getStringList("sources").asScala.toList.map(_.toUpperCase())
